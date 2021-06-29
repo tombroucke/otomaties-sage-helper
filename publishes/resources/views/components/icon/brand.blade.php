15852e69d7a151d27d68ea67993d899fe31b94c9
Tom Broucke <tom@tombroucke.be>
2021-06-29 23:08:47 +0200

    added icon

---
+++ publishes/resources/views/components/icon/brand.blade.php
@@ -0,0 +1,1 @@
+<i class="fab fa-{{ $name }}"></i>
--- publishes/resources/views/partials/language-switcher.blade.php
+++ publishes/resources/views/partials/language-switcher.blade.php
@@ -2,7 +2,7 @@
   <li class="menu-item menu-item--has-submenu {{ $classes ?? '' }} menu-item--language-switcher">
     <span class="menu-item__link">
       <img src="@asset('images/' . strtolower( $activeLanguage['tag'] ) . '.png')" alt="{!! $activeLanguage['native_name'] !!}">{!! $activeLanguage['native_name'] !!}
-      <i class="fa fa-chevron-down"></i>	
+      <x-icon name="chevron-down fa-xs" />	
     </span>
     <ul class="submenu">
       @foreach ($languages as $language)
