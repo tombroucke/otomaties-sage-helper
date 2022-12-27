4c2589276ce1dae6e345b330c24322abfa784da6
tombroucke <tom@tombroucke.be>
2022-12-27 10:28:13 +0100

    Update VerticalAlign.php

--- publishes/app/Blocks/Concerns/VerticalAlign.php
+++ publishes/app/Blocks/Concerns/VerticalAlign.php
@@ -7,6 +7,9 @@ trait VerticalAlign
     public function verticalAlignClass()
     {
         $class = '';
+        if (!is_object($this->block) || !$this->block->align_content) {
+            return $class;
+        }
         switch ($this->block->align_content) {
             case 'center':
                 $class = 'align-items-center';
