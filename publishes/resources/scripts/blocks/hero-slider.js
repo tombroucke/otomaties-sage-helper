40ab3bee6b79d688fcaf85914c44f5d6221e66a8
tombroucke <tom@tombroucke.be>
2023-01-19 16:37:47 +0100

    Create hero-slider.js

---
+++ publishes/resources/scripts/blocks/hero-slider.js
@@ -0,0 +1,41 @@
+import {domReady} from '@roots/sage/client';
+import Swiper, { Navigation, Pagination, Autoplay } from 'swiper';
+
+/**
+ * editor.main
+ */
+const main = (err) => {
+  if (err) {
+    // handle hmr errors
+    console.error(err);
+  }
+
+  Swiper.use([Navigation, Pagination, Autoplay]);
+
+  new Swiper('.wp-block-hero-slider .swiper', {
+    // Optional parameters
+    loop: true,
+    height: 500,
+    spaceBetween: 30,
+    autoplay: {
+      delay: 4500,
+      disableOnInteraction: false,
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+  });
+};
+
+/**
+ * Initialize
+ *
+ * @see https://webpack.js.org/api/hot-module-replacement
+ */
+domReady(main);
+import.meta.webpackHot?.accept(main);
