b2d022692ba5f9146ae57f6754b59abe91956c35
tombroucke <tom@tombroucke.be>
2023-11-14 21:12:26 +0100

    Update subpages.blade.php

--- publishes/resources/views/blocks/subpages.blade.php
+++ publishes/resources/views/blocks/subpages.blade.php
@@ -1,12 +1,17 @@
-<x-block :block="$block">
-	@if ($subpages)
-	  <x-list-group>
-		@foreach ($subpages as $subpage)
-		  <x-list-group.link href="{{ get_permalink($subpage->ID) }}">
-			{!! $subpage->post_title !!}
-		  </x-list-group.link>
-		@endforeach
-	  </x-list-group>
-	@endif
-</x-block>
-  
+@if ($subpages)
+	<x-block :block="$block">
+		  <x-list-group>
+			@foreach ($subpages as $subpage)
+			  <x-list-group.link href="{{ get_permalink($subpage->ID) }}">
+				{!! $subpage->post_title !!}
+			  </x-list-group.link>
+			@endforeach
+		  </x-list-group>
+	</x-block>
+@else
+  @if($block->preview)
+    <p>{{ __('No subpages found for this page', 'sage') }}</p>
+  @else
+    <!-- {{ __('No subpages found for this page', 'sage') }} ... -->
+  @endif
+@endif
